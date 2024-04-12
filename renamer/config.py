8f53192c5509555707a76d24cc2bb1bfce64968b
Pankaj Sharma <109496259+Eric138e@users.noreply.github.com>
2024-04-12 10:03:31 +0530

    Update config.py

--- renamer/config.py
+++ renamer/config.py
@@ -4,14 +4,14 @@ logger = logging.getLogger(__name__)
 
 
 class Config:
-    API_ID = int(os.environ.get("API_ID", 12345))
-    API_HASH = os.environ.get("API_HASH")
-    OWNER_ID =  int(os.environ.get("OWNER_ID", ""))
+    API_ID = int(os.environ.get("API_ID", "25341679"))
+    API_HASH = os.environ.get("API_HASH", "1a0656fa7566a46075cfe3f21676424c")
+    OWNER_ID =  int(os.environ.get("OWNER_ID", "7173269918"))
     AUTH_USERS = list(int(i) for i in os.environ.get("AUTH_USERS", "").split(" ")) if os.environ.get("AUTH_USERS", "") else []
     if OWNER_ID not in AUTH_USERS:
         AUTH_USERS.append(OWNER_ID)
     BANNED_USERS = [int(i) for i in os.environ.get("BANNED_USERS", "").split(" ")] if os.environ.get("BANNED_USERS", "") else None
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6966212073:AAFyS64VJhonSo6LTLl3SI2JE_UL2bw6M1c")
     BOT_PASSWORD = os.environ.get("BOT_PASSWORD", "") if os.environ.get("BOT_PASSWORD", "") else None
     CUSTOM_CAPTION = os.environ.get("CUSTOM_CAPTION") if os.environ.get("CUSTOM_CAPTION", "") else None
     FORCE_SUB = os.environ.get("FORCE_SUB", "") if os.environ.get("FORCE_SUB", "") else None
